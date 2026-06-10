4ddc8c4117a11d394485f41966b0bc1f24799907
Eddie Char <emchar1@gmail.com>
2026-06-10 08:43:24 -0700

    Refactor.

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,7 +4,7 @@ class_name Enemy
 # PROPERTIES
 
 enum Type {
-	ZOMBIE, SKELETON, FRANKENSTEIN, SLIME, BAT
+	ZOMBIE, SKELETON, SKELETON2, FRANKENSTEIN, SLIME, BAT
 }
 
 signal died
@@ -18,43 +18,29 @@ signal died
 @onready var player_detector = $PlayerDetector
 @onready var ground_ray = $RayCast2D
 
+var timer: Timer
+var tween: Tween
+
 var orig_speed = 50.0
 var dir := 1.0
 var is_on_ground := false
 var was_on_ground := false
-var timer: Timer
-var tween: Tween
 
 
-# FUNCTIONS
+# INIT FUNCTIONS
 
 func _ready() -> void:
 	pass
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	
-	# Handle movement
-	if is_on_floor():
-		velocity.x = speed * dir
-	
-	# Check raycasting
-	was_on_ground = is_on_ground
-	is_on_ground = ground_ray.is_colliding()
-	
-	# Update positions
-	ground_ray.target_position.x = abs(ground_ray.target_position.x) * dir
-	sprite.flip_h = dir < 0
-	
-	if was_on_ground and not is_on_ground:
-		change_directions()
-	
-	move_and_slide()
-	handle_collisions()
+	_handle_movement(delta)
+	_handle_collisions()
 
+	move_and_slide()
+
+
+# CONFIG FUNCTIONS
 
 func set_direction(player_pos: Vector2):
 	dir = -1 if global_position > player_pos else 1
@@ -62,72 +48,6 @@ func set_direction(player_pos: Vector2):
 
 func change_directions():
 	dir *= -1
-
-
-# TODO: - build this out, add hurt animation, death, etc.
-func hurt_enemy(dmg: int):
-	hp -= dmg
-	if hp <= 0:
-		kill_enemy()
-		return
-	
-	if tween:
-		tween.kill()
-	
-	tween = create_tween()
-	
-	for i in range(20):
-		tween.tween_property(self, "modulate", Color.DEEP_PINK, 0.05)
-		tween.tween_property(self, "modulate", Color.DARK_CYAN, 0.05)
-	
-	speed = 0
-	AudioManager.play(AudioData.AudioKey.ATTACK_SWING)
-	
-	if timer:
-		timer.stop()
-		timer.queue_free()
-		timer = null
-	
-	timer = Timer.new()
-	timer.wait_time = 0.6
-	timer.one_shot = true
-	add_child(timer)
-	timer.start()
-	
-	await timer.timeout
-	
-	if tween:
-		tween.kill()
-	
-	modulate = Color.WHITE
-	speed = orig_speed
-
-
-func kill_enemy():
-	if tween:
-		tween.kill()
-	
-	if timer:
-		timer.stop()
-		timer.queue_free()
-		timer = null
-	
-	modulate = Color.BLACK
-	speed = 0
-	sprite.stop()
-	AudioManager.play(AudioData.AudioKey.ATTACK_KILL)
-	
-	timer = Timer.new()
-	timer.wait_time = 0.2
-	timer.one_shot = true
-	add_child(timer)
-	timer.start()
-	
-	await timer.timeout
-	
-	CurseManager.increment_curse()	
-	died.emit()
-	queue_free()
 
 
 func configure_enemy(_type: Type):
@@ -143,6 +63,11 @@ func configure_enemy(_type: Type):
 			sprite.play("skeleton")
 			speed = 75
 			hp = 2
+			attack_dmg = 1
+		Type.SKELETON2:
+			sprite.play("skeleton2")
+			speed = 75
+			hp = 3
 			attack_dmg = 1
 		Type.FRANKENSTEIN:
 			sprite.play("frankenstein")
@@ -163,9 +88,74 @@ func configure_enemy(_type: Type):
 	orig_speed = speed
 
 
-# SIGNAL FUNCTIONS
+# DAMAGE FUNCTIONS
 
-func handle_collisions():
+func hurt_enemy(dmg: int):
+	hp -= dmg
+	if hp <= 0:
+		kill_enemy()
+		return
+	
+	_reset_timers()
+	_setup_timer(0.6, true)
+
+	speed = 0
+	AudioManager.play(AudioData.AudioKey.ATTACK_SWING)
+
+	tween = create_tween()
+	for i in range(20):
+		tween.tween_property(self, "modulate", Color.DEEP_PINK, 0.05)
+		tween.tween_property(self, "modulate", Color.DARK_CYAN, 0.05)
+	
+	await timer.timeout
+	
+	if tween:
+		tween.kill()
+	
+	modulate = Color.WHITE
+	speed = orig_speed
+
+
+func kill_enemy():
+	_reset_timers()
+	_setup_timer(0.2, true)
+
+	modulate = Color.BLACK
+	speed = 0
+	sprite.stop()
+	AudioManager.play(AudioData.AudioKey.ATTACK_KILL)
+	
+	await timer.timeout
+	
+	CurseManager.increment_curse()	
+	died.emit()
+	queue_free()
+
+
+# HELPER FUNCTIONS
+
+func _handle_movement(delta: float):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	# Handle movement
+	if is_on_floor():
+		velocity.x = speed * dir
+	
+	# Check raycasting
+	was_on_ground = is_on_ground
+	is_on_ground = ground_ray.is_colliding()
+	
+	# Update positions
+	ground_ray.target_position.x = abs(ground_ray.target_position.x) * dir
+	sprite.flip_h = dir < 0
+	
+	if was_on_ground and not is_on_ground:
+		change_directions()
+
+
+func _handle_collisions():
 	var bodies = player_detector.get_overlapping_bodies()
 	
 	for body in bodies:
@@ -173,8 +163,28 @@ func handle_collisions():
 			var player = body as Player
 			if player:
 				player.hurt_player(attack_dmg, global_position)
-				print("Enemy hurt player.")
 
+
+func _reset_timers():
+	if timer:
+		timer.stop()
+		timer.queue_free()
+		timer = null
+	
+	if tween:
+		tween.kill()
+		tween = null
+
+
+func _setup_timer(wait_time: float, one_shot: bool):
+	timer = Timer.new()
+	timer.wait_time = wait_time
+	timer.one_shot = one_shot
+	add_child(timer)
+	timer.start()
+
+
+# SIGNAL FUNCTIONS
 
 func _on_player_detector_body_entered(_body: Node2D) -> void:
 	pass
