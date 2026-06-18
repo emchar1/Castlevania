23a6e176b16ff003ddceba1804d08a79607dae32
Eddie Char <emchar1@gmail.com>
2026-06-17 23:48:21 -0700

    Add cursed_spawners and add to forest0...3.

---
+++ level_maps/cursed_spawners.gd
@@ -0,0 +1,55 @@
+extends Node2D
+class_name CursedSpawners
+
+# PROPERTIES
+
+@export var a_spawners_type: Enemy.Type = Enemy.Type.ZOMBIE
+@export var b_spawners_type: Enemy.Type = Enemy.Type.ZOMBIE
+@export var c_spawners_type: Enemy.Type = Enemy.Type.ZOMBIE
+@export var d_spawners_type: Enemy.Type = Enemy.Type.ZOMBIE
+
+@onready var spawners_a: Array = [
+	$SpawnManager3, $SpawnManager11, $SpawnManager19, $SpawnManager27
+]
+
+@onready var spawners_b: Array = [
+	$SpawnManager4, $SpawnManager12, $SpawnManager20, $SpawnManager28
+]
+
+@onready var spawners_c: Array = [
+	$SpawnManager5, $SpawnManager13, $SpawnManager21, $SpawnManager29
+]
+
+@onready var spawners_d: Array = [
+	$SpawnManager6, $SpawnManager14, $SpawnManager22, $SpawnManager30
+]
+
+
+# FUNCTIONS
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_setup_spawners()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _setup_spawners():
+	if a_spawners_type != Enemy.Type.ZOMBIE:
+		for spawner in spawners_a:
+			spawner.enemy_type = a_spawners_type
+	
+	if b_spawners_type != Enemy.Type.ZOMBIE:
+		for spawner in spawners_b:
+			spawner.enemy_type = b_spawners_type
+	
+	if c_spawners_type != Enemy.Type.ZOMBIE:
+		for spawner in spawners_c:
+			spawner.enemy_type = c_spawners_type
+	
+	if d_spawners_type != Enemy.Type.ZOMBIE:
+		for spawner in spawners_d:
+			spawner.enemy_type = d_spawners_type
